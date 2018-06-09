2ccfef497418eb79708790d721fe58c8ddfe0d47
Codsir <littlebiao@outlook.com>
2018-06-09 14:21:19 +0800

    3th

--- optim.py
+++ optim.py
@@ -66,11 +66,11 @@ def ExactLineSearch_quadratic(f, x_k, p_k):
 
 def QuasiNewton(f, *x_0,  HUpdateMethod = 'BFGS', LineSearch = ExactLineSearch_quadratic, tolerance = 0.0001):
     if not hasattr(f, "paraLength"):
-        raise Exception("请确保目标函数f定义了f.paraLength属性, 为输入向量长度, \
-              即\"f.paraLength = 变量个数 \" ")
+        raise Exception("Please make sure that f has f.paraLength,  which is \
+              the length of x_0,that is \"f.paraLength = len(x)\" ")
     assert isfunction(f)
     input_size  = f.paraLength
-    # 初始化x_0
+    # initializing x_0
     if len(x_0) != 0:
         x_0 = np.mat(x_0[0]).T
     else:
@@ -81,7 +81,7 @@ def QuasiNewton(f, *x_0,  HUpdateMethod = 'BFGS', LineSearch = ExactLineSearch_q
     g_0 = getGrad(f, (x_0.T).tolist()[0])
     g_0 = np.mat(g_0).T
     LSfun = LineSearch
-    ## H_k update function 这里采用占位符实际上起到了一般函数的作用
+    ## H_k update function 
     s_k = tf.placeholder(tf.float32, shape=np.shape(x_0))
     y_k = tf.placeholder(tf.float32, shape=np.shape(x_0))
     H_k = tf.placeholder(tf.float32, shape=(input_size, input_size)) 
@@ -93,7 +93,7 @@ def QuasiNewton(f, *x_0,  HUpdateMethod = 'BFGS', LineSearch = ExactLineSearch_q
         update_2 = I_mat - tf.matmul(y_k, tf.transpose(s_k))/divider
         H_update = tf.matmul(tf.matmul(update_1, H_k), update_2) + update_3
     elif HUpdateMethod == 'DFP':
-        update_1 = tf.matmul(tfmatmul(tf.matmul(H_k, y_k), tf.transpose(y_k)), H_k)
+        update_1 = tf.matmul(tf.matmul(tf.matmul(H_k, y_k), tf.transpose(y_k)), H_k)
         update_2 = tf.matmul(tf.matmul(tf.transpose(y_k), H_k), y_k)
         H_update = H_k + update_3 - update_1/update_2
         
@@ -103,8 +103,6 @@ def QuasiNewton(f, *x_0,  HUpdateMethod = 'BFGS', LineSearch = ExactLineSearch_q
         print('The %d iteration...'% (k_num))
         p_0 = - np.dot(H_0, g_0)
         a_0 = LSfun(f, (x_0.T).tolist()[0], p_0)
-        print('a_0')
-        print(a_0)
         x_1 = x_0 + np.dot(p_0, a_0)
         g_1 = np.mat(getGrad(f, (x_1.T).tolist()[0])).T
         s_0 = x_1 - x_0
@@ -116,7 +114,7 @@ def QuasiNewton(f, *x_0,  HUpdateMethod = 'BFGS', LineSearch = ExactLineSearch_q
         H_0 = copy.deepcopy(H_1)
         print('x_%d:'%(k_num))
         print(x_0)
-        print('error = %f'%(np.linalg.norm(g_0,2)))
+        print('gradient at x_%d = %f'%(k_num, np.linalg.norm(g_0,2)))
         k_num += 1
         
     print('After %d iteration, the minimum point = ' % (k_num-1))
@@ -130,12 +128,12 @@ def QuasiNewton(f, *x_0,  HUpdateMethod = 'BFGS', LineSearch = ExactLineSearch_q
 
 def TrustRegion_dogleg(f, delta = 0.5, eta = 0, *x_0, tolerance= 0.0001):
     if not hasattr(f, "paraLength"):
-        raise Exception("请确保目标函数f定义了f.paraLength属性, \
-              为输入向量长度, 即\"f.paraLength = 变量个数\" ")
+        raise Exception("Please make sure that f has f.paraLength,  which is \
+              the length of x_0,that is \"f.paraLength = len(x)\" ")
     assert delta > 0
     assert isfunction(f)
     input_size  = f.paraLength
-    # 初始化x_0
+    # initializing x_0
     if len(x_0) != 0:
         pass
     else:
@@ -146,16 +144,17 @@ def TrustRegion_dogleg(f, delta = 0.5, eta = 0, *x_0, tolerance= 0.0001):
     g_k = getGrad(f, x_k) 
     k_num = 0
     while np.linalg.norm(g_k, 2) > tolerance:
+        print('The %d iteration...' % (k_num+1))
         g_k = getGrad(f, x_k) 
 #         print('g_%d:'%(k_num))
 #         print(g_k)
         g_k = np.mat(g_k).T
         g_k_square = tf.pow(tf.norm(g_k, 2), 2)
-        ## 注意这里把nump array转化为tf.matrix       
+        ##nump array to tf.matrix       
         H_k = np.mat(getHess(f, x_k))
 #         print('H_%d:'%(k_num))
 #         print(H_k)
-        print('H_k')
+        print('H_%d' %(k_num))
         print(H_k)
         denom = tf.matmul(tf.matmul(g_k.T, H_k), g_k)
         p_u = - g_k * g_k_square /denom
@@ -194,12 +193,12 @@ def TrustRegion_dogleg(f, delta = 0.5, eta = 0, *x_0, tolerance= 0.0001):
                 delta_k = min(2*delta_k, delta_hat)
         if rho_k > eta:
             x_k = x_k + p_k
-        print('x_%d' % (k_num))
+        print('x_%d' % (k_num+1))
         print(x_k)
-        print('f_%d = %f'% (k_num, f(x_k)))
-        print('tolerance_%d = %f' %(k_num, np.linalg.norm(g_k, 2))) 
+        print('f_%d = %f'% (k_num+1, f(x_k)))
+        print('gradient at x_%d = %f' %(k_num+1, np.linalg.norm(g_k, 2))) 
         k_num += 1
-        print('The %d iteration...' % (k_num))
+
     print('After %d times iteration, the Minimum point is:'%(k_num))
     print(x_k)
     print('and the Minimum value is %f' %(f(x_k)))
@@ -214,11 +213,11 @@ def PenaltySimple(f, c_eq, c_leq, *x_0, sigma = 0.01, alpha = 2, norm = 2, epsil
     '''
     #c_leq_new = []
     if not hasattr(f, "paraLength"):
-        raise Exception("请确保目标函数f定义了f.paraLength属性, \
-              为输入向量长度, 即\"f.paraLength = 变量个数\" ")
+        raise Exception("Please make sure that f has f.paraLength,  which is \
+              the length of x_0,that is \"f.paraLength = len(x)\" ")
     assert isfunction(f)
     input_size  = f.paraLength
-    # 初始化x_0
+    # initializing x_0
     if len(x_0) != 0:
         x_0 = x_0[0]
     else:
@@ -241,7 +240,7 @@ def PenaltySimple(f, c_eq, c_leq, *x_0, sigma = 0.01, alpha = 2, norm = 2, epsil
     sess = tf.Session()
     k_num = 0
     while c_xsigma_norm > epsilon:
-        ## 梯度下降法求罚函数最小值点
+        ## steepest gradient methos
         print('The %dth iteration...'%(k_num+1))
         grad_inside = tf.gradients(p_fun, x)
         hess_inverse = tf.matrix_inverse(tf.hessians(p_fun,x))
@@ -264,7 +263,7 @@ def PenaltySimple(f, c_eq, c_leq, *x_0, sigma = 0.01, alpha = 2, norm = 2, epsil
         c_xsigma_norm = sess.run(tf.norm(c_eq), feed_dict = {x:xsigma})
         sigma = 10*sigma
         k_num += 1
-        print('error = %f'% (c_xsigma_norm))
+        print('gradient at x_%d =  %f'% (k_num, c_xsigma_norm))
     print('After %d iteration, the minimum point = '%(k_num))    
     print(xsigma)
     print('the minimum value = %f' %(f(xsigma)))
@@ -279,28 +278,78 @@ def PenaltySimple(f, c_eq, c_leq, *x_0, sigma = 0.01, alpha = 2, norm = 2, epsil
     
     
 def main():    
+    
     ## Demo 1:trust region method with subproblems solved by the Dogleg method
     print('Demo 1:trust region method with subproblems solved by the Dogleg method')
     f = lambda x:100*(x[1]-x[0]**2)**2 + (1-x[0])**2
-    f.paraLength = 2    ## 这一步不可缺少
+    f.paraLength = 2   
 #     print(f.paraLength)
     x_k, f_k = TrustRegion_dogleg(f, delta = 10)
     
     ## Demo 2:quasi-Newton method demo
-    print('Demo 2:quasi-Newton method demo')
+    print('Demo 2:quasi-Newton method demo with BFGS')
     f = lambda x:x[0]**2 + 2 * x[1]**2
     f.paraLength = 2
     x_0 = np.array([1, 1])
-    x_k, f_k = QuasiNewton(f, x_0)
+    x_k, f_k = QuasiNewton(f, x_0, HUpdateMethod = 'BFGS')
     
-    ## Demo 3:penalty function method demo
-    print('Demo 3:penalty function method demo')
+    ## Demo 3:quasi-Newton method demo
+    print('Demo 3:quasi-Newton method demo with DFP')
+    f = lambda x:x[0]**2 + 2 * x[1]**2
+    f.paraLength = 2
+    x_0 = np.array([1, 1])
+    x_k, f_k = QuasiNewton(f, x_0, HUpdateMethod = 'DFP')
+
+    
+    ## Demo 4:penalty function method demo
+    print('Demo 4:penalty function method demo')
     f = lambda x:x[0] + x[1]
     f.paraLength = 2
     c_eq = [lambda x:x[0]**2 + x[1]**2 - 2]
     c_leq = []
     x_k, f_k = PenaltySimple(f, c_eq, c_leq, [-3,-4])
     #if len(c_leq) > 0:        
+    
+    ## Demo 5;CG method with exact line method
+    print('Demo 5;CG method with exact line method')
+    A = np.mat([[4, 1], [1,3]])
+    print('A')
+    print(A)
+    x_0 = np.mat([2,1]).T
+    print('x_0')
+    print(x_0)
+    b_vec = np.mat([-1, -2]).T
+    print('b')
+    print(b_vec)
+    g_0 = A.T * x_0 + b_vec
+    print('g_0')
+    print(g_0)
+    d_0 = -g_0
+    print('d_0')
+    print(d_0)
+    beta_0 = 0
+    k_num = 1
+    while np.linalg.norm(g_0, 2) > 0.0001:
+        print('The %dth iteration'%(k_num))
+        alpha_0 = -(g_0.T * d_0)/(d_0.T * A * d_0)
+        print('alpha_%d' %(k_num))
+        print(alpha_0)
+        x_1 = x_0 + d_0 * alpha_0
+        print('x_%d'%(k_num))
+        print(x_1)
+        g_1 = A.T * x_1 + b_vec
+        print('g_%d'%(k_num))
+        print(g_1)
+        beta_1 =  (g_1.T * A * d_0)/(d_0.T * A * d_0)
+        print('beta_%d'%(k_num))
+        print(beta_1)
+        d_1 = -g_1 + d_0* beta_1 
+        g_0 = g_1
+        d_0 = d_1
+        beta_0 = beta_1
+        x_0 = x_1
+        print('norm of gradients = %f'%(np.linalg.norm(g_0, 2)))
+        print('f_%d = %f' %(k_num, 1/2 * x_0.T * A * x_0+b_vec.T*x_0))
         
 if __name__ == "__main__":
     main()
